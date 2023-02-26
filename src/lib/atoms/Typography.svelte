68e50fabb3dd38947e36896730693031eeaaf7b6
Adrian Piątek <adr654231@gmail.com>
2023-02-26 16:30:31 +0100

    Add Typography component

---
+++ src/lib/atoms/Typography.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  type TypographyTag = "span" | "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "div" | "a";
+  type TypographyWeight = "light" | "normal" | "medium" | "semibold" | "bold";
+
+  export let tag: TypographyTag = "span";
+  export let weight: TypographyWeight = "normal";
+  export let center = false;
+</script>
+
+<svelte:element this={tag} class="font-{weight} text-{center ? 'center' : 'left'} {$$props.class}">
+  <slot /></svelte:element
+>
