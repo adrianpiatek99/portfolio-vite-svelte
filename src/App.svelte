f107c248a8e7490fc72ba4f4906c63b195e3099b
Adrian Piątek <adr654231@gmail.com>
2023-02-26 16:47:12 +0100

    Add AboutMeSection

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,13 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
-
-  import svelteLogo from "./assets/svelte.svg";
+  import AboutMeSection from "lib/AboutMeSection.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class="relative flex flex-col min-h-[200vh]">
+  <!-- <Header /> -->
+  <div
+    class="absolute inset-0 max-h-[67vh] bg-gradient-radial bg-[length:30px_30px] bg-[0_0,15px_15px] z-[-1px] after:content-[''] after:absolute after:left-0 after:right-0 after:bottom-0 after:h-[500px] after:bg-linear-gradient before:content-[''] before:absolute before:left-0 before:right-0 before:top-0 before:h-[50px] before:bg-gradient-to-b before:from-[#060606] before:to-[transparent]"
+  />
+  <div class="flex flex-col max-w-[1240px] w-full mx-auto p-[30px] z-1">
+    <AboutMeSection />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ src/lib/Section.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let id: string;
+  export let headingText: string;
+</script>
+
+<section class="flex flex-col" {id}>
+  <div class="flex flex-col gap-[40px] w-full my-[84px] mx-auto sm:my-[124px] lg:flex-row">
+    <div class="animate-hidden flex flex-col w-full h-full lg:sticky lg:top-[70px]">
+      <h2 class="flex font-semibold text-5xl sm:text-7xl">
+        {headingText}
+      </h2>
+    </div>
+    <div class="flex flex-col w-full gap-[30px] mt-[7px]">
+      <slot />
+    </div>
+  </div>
+</section>
