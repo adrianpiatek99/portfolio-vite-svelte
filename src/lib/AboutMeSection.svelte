153eaba3102f9618233d19a355e63caad8607c87
Adrian Piątek <adr654231@gmail.com>
2023-02-26 16:50:29 +0100

    feat: Add about me section (#3)
    * Add links
    
    * Add AboutMeSection
    
    * Update about me description

---
+++ src/lib/AboutMeSection.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Section from "./Section.svelte";
+  import IconButton from "atoms/IconButton/IconButton.svelte";
+  import Typography from "atoms/Typography.svelte";
+
+  import githubLogo from "assets/icons/github.svg";
+  import { GITHUB_URL } from "constants/links";
+</script>
+
+<Section id="about-me" headingText="Adrian Piątek">
+  <div class="animate-hidden flex flex-col w-full gap-[30px]">
+    <Typography tag="p" class="text-primary text-xl sm:text-2xl" weight="semibold"
+      >Junior React Developer</Typography
+    >
+    <Typography tag="p" class="text-color2 text-l sm:text-xl whitespace-pre-line"
+      >{"Hello and welcome to my portfolio!\n Passionate about web development and a bit of design. My journey with web dev development started at the turn of 2020 and 2021."}</Typography
+    >
+    <div class="flex gap-[12px]">
+      <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer" tabIndex={-1}>
+        <IconButton title="GitHub" size="large">
+          <img draggable="false" src={githubLogo} alt="GitHub" />
+        </IconButton>
+      </a>
+    </div>
+  </div>
+</Section>
